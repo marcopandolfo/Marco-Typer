cf9e7e1a72e64faf95240f153c18effbd70d62f0
Marco Antonio <marcopandolfo577@gmail.com>
2018-11-27 18:34:27 -0200

    Animando o scroll

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,3 +1,5 @@
+$("#botao-placar").click(mostraPlacar);
+
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Marco";
@@ -7,6 +9,16 @@ function inserePlacar() {
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.prepend(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar() {
+    var posicaoPlacar = $(".placar").offset().top;
+    $("html").animate(
+    {
+        scrollTop: posicaoPlacar + "px"
+    }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
@@ -30,5 +42,13 @@ function novaLinha(usuario, palavras) {
 
 function removeLinha()  {
     event.preventDefault();
-    $(this).parent().parent().remove();
+    var linha = $(this).parent().parent()
+    linha.fadeOut(1000);
+    setTimeout(function() {
+        linha.remove();
+    }, 1000);
+}
+
+function mostraPlacar() {
+    $(".placar").stop().slideToggle(600);
 }
