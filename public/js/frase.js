563a367cdf30aa90f38695e0f0a29c2c1e2da5b7
Marco Antonio <marcopandolfo577@gmail.com>
2018-12-01 16:23:04 -0200

    Enviando dados(parametros) na requisicao get do ajax

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,18 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
-function fraseAleatoria(params) {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+function fraseAleatoria() {
+    $("#spinner").toggle();
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function() {
+            $("#erro").toggle();
+        }, 2000);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
 }
 
 function trocaFraseAleatoria(data) {
@@ -10,4 +21,29 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    console.log("Id da minha frase: " + fraseId);
+    
+    var dados = {id: fraseId};
+
+    $.get("http://localhost:3000/frases",dados, trocaFrase)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function() {
+            $("#erro").toggle();
+        }, 2000);
+    })
+    .always(function() {
+        $("#spinner").toggle();
+    });
+}
+function trocaFrase(data) {
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
